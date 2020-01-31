9ab5c1f49bd416ce34a413d1963bde7b28127c2b
Giorgi Coelho <giorgi_coelho@hotmail.com>
2020-01-31 10:17:43 -0300

    Primeiros passos no desenvolvimento do webapp de construção de um burguer.

---
+++ src/containers/BurguerBuilder/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class BurguerBuilder extends Component {
+    render() {
+        return (
+            <>
+            <div>Burguer</div>
+            <div>Build Controls</div>
+            </>
+        );
+    }
+}
